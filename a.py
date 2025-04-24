5c7757385c3b84e1de214263e6d7ccce32b15b3b
WillSantosNe <williamsantosstella@gmail.com>
2025-04-24 18:29:03 -0300

    removendo comentarios2

--- a.py
+++
@@ -1,8 +0,0 @@
-nome = input("olá, qual é seu nome?: ")
-input(f"olá {nome}, é um prazer te conhecer, posso fazer conta de multiplicação para você! É  sé apertar ENTER para começar! ")
-numero1 = int(input(f"fale o primeiro valor: "))
-numero2 = int(input(f"Ok, agora, fale o valor que quer multiplicar com {numero1}?: "))
-resultado = (numero1)*(numero2)
-print(f"Então {nome}, o valor de ({numero1}) multiplicado por ({numero2}) ({numero1}X{numero2}) é ({resultado})")
-
-
